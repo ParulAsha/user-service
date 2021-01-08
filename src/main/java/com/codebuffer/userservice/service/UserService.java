2aff71e6f079d91446d2ba2434b4194e99c7dbf4
Parul Anand <parulanand30@gmail.com>
2021-01-08 08:12:33 +0530

    git commit -m "second commit"

--- src/main/java/com/codebuffer/userservice/service/UserService.java
+++ src/main/java/com/codebuffer/userservice/service/UserService.java
@@ -29,7 +29,7 @@ public class UserService {
         ResponseTemplateVO responseTemplateVO=new ResponseTemplateVO();
         User user=userRepository.findByUserId(userId);
 
-        Department department=restTemplate.getForObject("http://localhost:8080/departments/"+user.getDepartmentId(),Department.class);
+        Department department=restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/"+user.getDepartmentId(),Department.class);
         responseTemplateVO.setDepartment(department);
         responseTemplateVO.setUser(user);
         return responseTemplateVO;
